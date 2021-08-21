09c502aa0183b71069df254e95820b71440b0823
Juanita Patrick <juanitapatrick@Juanitas-iMac.attlocal.net>
2021-08-21 11:37:22 -0500

    pseudocoding game logic

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-// console.log('Helllllllooooooooo!!!!');
+//FOCUS ON QUESTION 1
+//Create spongebob fact and answer choices
+//make all incorrect answers equal zero points
+//make the one correct answer equal 50 points
+
+
+
+
+let playerAnswer;
+const wrongAnswer = 0;
+const rightAnswer = 50;
+//const userWins = the final total after all points are calculated in the end
+
+if ()
